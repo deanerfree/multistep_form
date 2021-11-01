04202296085fc2907f511c41cee88f92bb2bef5d
Kurt Ball <kurtisdball@hotmail.com>
2021-11-01 02:43:49 -0600

    further edit

--- src/components/Form.js
+++ src/components/Form.js
@@ -36,7 +36,7 @@ export default function UserForm() {
 		}
 
 		axios
-			.post(`${process.env.API_CONNECTION}/api/create`, values)
+			.post(`${process.env.API_CONNECTION}`, values)
 			.then((resp) => {
 				if (resp.status === 200) {
 					setResponseData(resp)
